0b8000cf3c76ea3e0d91b8fcf398ae55666c0ece
yulhee741 <dbfgml980413@gmail.com>
2021-09-20 00:51:26 +0900

    entity class 1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,25 +3,24 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Entity
+@Entity
 @Getter @Setter
 public class Member {
 
-//    @Id @GeneratedValue
-//    @Column(name = "member_id")
-//    private Long id;
-//
-//    private String name;
-//
-//    private Address address;
-//
-//    private List<Order> orders = new ArrayList<>();
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
 }
